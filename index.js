fb95ef4790a9f1c0af90507d442ff665e92fd841
Matheus Firmino <68831054+Matheus-2211@users.noreply.github.com>
2020-12-30 11:41:47 -0300

    aula 02

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require('express'); // eu estou importando o modulo chamado expr
 
 const app = express(); //criação de uma varialvel que vai amazenar minha aplicação
 
-app.get('/', (request, response) => { // primeira rota
+app.use(express.json()); //informando o meu app que estou usando o json para as requisições, é para o express ir la no corpo da minha requisição, e converter o json em objeto entendivel pelo JS.
+
+
+app.post('/users', (request, response) => { // primeira rota // o request guarda todos os dados vindo na minha requisição
+    const body = request.body;
+
+    console.log(body);
+
     return response.json({
         saudação: 'Olá, Mundo',
-        message: 'Me Livrei da Maldição!!'
+        name: 'Matheus'
     });
 });
 
