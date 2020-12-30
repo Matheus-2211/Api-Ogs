eb06083de721dc5ac6737e62a31be159e2189234
Matheus Firmino <68831054+Matheus-2211@users.noreply.github.com>
2020-12-30 13:25:24 -0300

    criado a tabela

---
+++ src/database/migrations/20201230124310_create_ongs.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('ongs', function(table) { //criado uma tabela para aplicação
+        table.string('id').primery();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('uf').notNullable();
+    });
+
+};
+
+exports.down = function(knex) {
+    return knex.schema.droptable('ongs');
+
+};
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,13 @@ const express = require('express');
 
 const routes = express.Router(); // Criado uma varia chamdo routes, ou seja eu estou desacoplando modo de rotas do express em uma nova varialvel
 
-routes.post('/users', (request, response) => { // primeira rota // o request guarda todos os dados vindo na minha requisição
-    const body = request.body;
+routes.post('/ongs', (request, response) => { // primeira rota // o request guarda todos os dados vindo na minha requisição
+    const { name, email, whatsapp, city, uf } = request.body;
 
-    console.log(body);
+    console.log(data);
 
     return response.json({
-        saudação: 'Olá, Mundo',
-        name: 'Matheus'
+
     });
 });
 
