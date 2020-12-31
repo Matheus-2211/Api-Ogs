b7b586ba39b1914393c7ea24e6f7887fda2b377c
Matheus Firmino <68831054+Matheus-2211@users.noreply.github.com>
2020-12-31 10:00:56 -0300

    Rotas de casos
    Criado as rotas para  os casos
    1º rota de listgem de casos
    2º rota de cadastro de casos
    3º rota de deletar casos

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,26 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+
 
 const routes = express.Router(); // Criado uma varia chamdo routes, ou seja eu estou desacoplando modo de rotas do express em uma nova varialvel
+
 
 //Rota listagem de ONGS
 routes.get('/ongs', OngController.index);
 
-
 // Rota cadastro de ONGS
 routes.post('/ongs', OngController.create); // primeira rota // o request guarda todos os dados vindo na minha requisição
+
+//Rota de listagem de casos
+routes.get('/incidents', IncidentController.index);
+
+//rota de cadastro de casos
+routes.post('/incidents', IncidentController.create);
+
+routes.delete('/incidents/:id', IncidentController.delete);
+
 
 
 module.exports = routes; // exportando as minhas rotas para o index;
